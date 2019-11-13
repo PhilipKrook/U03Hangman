5bca5c6e5d089c998b25e7877b68cdf87a49928e
Philip Krook <philip.krook@chasacademy.se>
2019-11-13 19:45:15 +0100

    made stuff work better, and added the letters to the boxes

--- js/script.js
+++ js/script.js
@@ -1,111 +1,77 @@
-// Global variables
-let correctLetters = 0;
-let guesses = 0;
-let selectedWord = '';
+// Globala variabler
 
-// Array: whith the words
-const wordList = ["HELLO", "GOODBYE", "HAAAAANGMAAAN"];
+// Array: whith the words 
+const wordList = ['hello', 'goodbye', 'philiprocks'];
+
+// String: with THE word the generator selects
+let selectedWord = ''; // generateRandomWord();
+
+// Number: stores the numbers of guesses
+let guesses = 0;
 
 // String: searchpath to imgage (and changes) wrong answer. ex. `/images/h1.png`
-/*
-let hangmanImg = `images/h${guesses}.png`;
-let imageElement = document.querySelector("#hangman");
-imageElement.setAttribute("src", hangmanImg);
-*/
+let hangmanImg = `images/h${guesses}.png`
+let imageElement = document.querySelector('#hangman')
+imageElement.setAttribute('src', hangmanImg);
 
 // DOM-node: logs a messange when the game is over
-let msgHolderEl = "Game Over";
+let msgHolderEl;     
 
 // DOM-node: the button that starts the game
-let startGameBtnEl = document.querySelector("#startGameBtn");
-wordBoxes = document.querySelector("#letterBoxes > ul");
+let startGameBtnEl = document.querySelector('#startGameBtn');
+wordBoxes = document.querySelector('#letterBoxes > ul');
 
 // Array of DOM-nodes: the buttons for the letters
-let letterBoxEls = document.querySelector("#letterButtons").children;
+let letterBoxEls = document.querySelector('#letterButtons').children;
+for (var i = 0; i < letterBoxEls.length; i++) {
+    (function(i) {
+        letterBoxEls[i].addEventListener('click', event => {
+        console.log(letterBoxEls[i].children[0].value);
+    })})(i);    
+}
+
+// Array of DOM-nodes: the squares which holds the letters
+// let letterBoxEls;    
 
 // Functions to start the game with buttonclick, and then calls other functions
-startGameBtnEl.addEventListener("click", startGame);
-
-// ---------- Behold, the functions! ----------
+startGameBtnEl.addEventListener('click', startGame);
 
 // Function which randomly selects a word
 function generateRandomWord() {
-  return wordList[Math.floor(Math.random() * wordList.length)];
-}
+    return wordList[Math.floor(Math.random()*wordList.length)];
+};
 
 // Function to generate the letterboxes, number of boxes depends on the number of letters in the word
 function createLetterBoxes() {
-  for (i = 0; i < selectedWord.length; i++) {
-    let newElement = document.createElement("li");
-    let newInput = document.createElement("input");
-    newInput.setAttribute("type", "text");
-    newInput.setAttribute("disabled", "");
-    newElement.appendChild(newInput);
-    wordBoxes.appendChild(newElement);
-  }
-}
+    for ( i = 0; i < selectedWord.length; i++ ) {
+    		let newElement = document.createElement('li')
+        let newInput = document.createElement('input')
+        newInput.setAttribute('type', 'text')
+        newInput.setAttribute('disabled', '')
+        newElement.appendChild(newInput);
+        wordBoxes.appendChild(newElement);
+    };    
+};
 
-// Function for start game and select a random word
 function startGame() {
-  let list = document.querySelector("#letterBoxes > ul");
-  list.innerHTML = "";
-  selectedWord = generateRandomWord();
-  createLetterBoxes();
-  let wordSplit = selectedWord.split("");
-  console.log(wordSplit);
-
-  addLetterClickHandlers(selectedWord);
+    let list = document.querySelector('#letterBoxes > ul');
+    list.innerHTML = '';
+    selectedWord = generateRandomWord();
+    createLetterBoxes()
 }
+
 
 // Function that runs when you press a letters and guesses the letter
-function checkLetter(letter, selectedWord) {
-  let letters = selectedWord.split("");
-  console.log(letter);
-  console.log(selectedWord);
-  if (letters.indexOf(letter) !== -1) {
-    correctGuessAction(letter, letters);
-  } else {
-    wrongGuessAction();
-  }
-  console.log(letter);
-}
-
-// Function for correct answers
-function correctGuessAction(letter, letters) {
-  let occurances = [];
-  for (let i = 0; i < letters.length; i++) {
-    if (letter == letters[i]) {
-      correctLetters++;
-      //add the letters to the boxes somehow
-      //and print "win" when the word is full. selectedWord.length == correctLetters perhaps?
-    }
-  }
-}
-
-// Function for wrong answers and pictures
-function wrongGuessAction() {
-  guesses++;
-  // for testing
-  console.log(guesses);
-  if (guesses <= 6) {    
-    let hangmanImg = `images/h${guesses}.png`;
-    let imageElement = document.querySelector("#hangman");
-    imageElement.setAttribute("src", hangmanImg);    
-  } if (guesses == 6) {
-    // game over
-    console.log(msgHolderEl);    
-  }
-}
-
-// Function for click handlers
-function addLetterClickHandlers(selectedWord) {
-  for (var i = 0; i < letterBoxEls.length; i++) {
-    letterBoxEls.item(i).children[0].addEventListener("click", event => {
-      event.srcElement.setAttribute("disabled", "");
-      checkLetter(event.srcElement.value, selectedWord);
-    });
-  }
-}
-
 // Function that gets called by winning or loosing, does different things depending on status
 // Function wthich inaktiates/activates the letterbuttons depending on which part of the game you are on
+
+// to do
+// 1 click button
+// -check if the letter is inside the word
+// -we disable this button
+// -if it is inside the word - show box
+// -if its not in the word set -1 on the tries
+// the images
+// button click callback
+
+// -check if guess the whole word   -> WON
